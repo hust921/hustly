1050253483cba64d55578195af5bf783c80cf397
Morten Lund <mlairsup@outlook.com>
2019-12-22 17:45:38 +0100

    Added .args template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = App::new("hustly dotfiles installer")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Personal dotfiles installer")
+        .arg(
+            Arg::with_name("Version")
+                .short("V")
+                .long("version")
+                .help("Print version info and exit"),
+        )
+        .arg(
+            Arg::with_name("Verbose")
+                .short("v")
+                .long("verbose")
+                .help("Use verbose output (-vv very verbose)")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("Quiet")
+                .short("q")
+                .long("quiet")
+                .help("No output printed to stdout"),
+        )
+        .arg(
+            Arg::with_name("Dry-Run")
+                .short("n")
+                .long("dry-run")
+                .help("Run without execution"),
+        )
+        .arg(
+            Arg::with_name("User")
+                .short("u")
+                .long("user")
+                .help("Only install for current user"),
+        )
+        .arg(
+            Arg::with_name("SystemInstall")
+                .long("all")
+                .help("Install for all users on system"),
+        )
+        .get_matches();
 }
